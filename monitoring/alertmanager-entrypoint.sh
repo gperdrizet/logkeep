a0b1eb668a08711c31d58abcbb3d179527c4b40b
George Perdrizet <george@perdrizet.org>
2025-12-19 14:50:09 -0500

    Replace envsubst with sed in Alertmanager entrypoint

--- monitoring/alertmanager-entrypoint.sh
+++ monitoring/alertmanager-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Process the Alertmanager config template with environment variables
-envsubst < /etc/alertmanager/alertmanager.yml.template > /etc/alertmanager/alertmanager.yml
+sed -e "s/\${SMTP_HOST}/${SMTP_HOST}/g" \
+    -e "s/\${SMTP_PORT}/${SMTP_PORT}/g" \
+    -e "s/\${SMTP_USER}/${SMTP_USER}/g" \
+    -e "s/\${SMTP_PASSWORD}/${SMTP_PASSWORD}/g" \
+    -e "s/\${ALERT_EMAIL}/${ALERT_EMAIL}/g" \
+    /etc/alertmanager/alertmanager.yml.template > /etc/alertmanager/alertmanager.yml
 
 # Start Alertmanager with the processed config
 exec /bin/alertmanager "$@"
